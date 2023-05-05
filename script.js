f6cb884ead6e4d4c250149889daa0816fcbfa294
Walter Leal <wdaniel9501@gmail.com>
2023-05-04 22:03:08 -0500

    Nueva pagina a main

---
+++ script.js
@@ -0,0 +1,11 @@
+// Get the button element
+const button = document.getElementById('change-color-button');
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+  // Get the body element
+  const body = document.querySelector('body');
+
+  // Change the background color
+  body.style.backgroundColor = '#44576D';
+});
